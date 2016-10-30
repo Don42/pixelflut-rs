ccf19a0aaf6994ca26e7f16cb9298ccf5dc45e57
Marco 'don' Kaulea <DonMarco42@gmail.com>
2016-10-30 21:26:33 +0100

    Implement rudimentary interface
    Clients can send pixels to port 1234 via TCP.
    The server interprets every seven bytes as one pixel, this means that
    every binary file is a valid stream of pixels.
    All integers are read in network byteorder and coordinates start in the
    top left corner.
    The first two bytes represent the x coordinate, the next two bytes the y
    coordinate. The next three bytes are interpret as the red, green and
    blue value of the pixel.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
-extern crate rand;
 extern crate piston;
 extern crate graphics;
 extern crate glutin_window;
 extern crate opengl_graphics;
 extern crate image as im;
+extern crate byteorder;
 
 use std::sync::{Arc, Mutex};
 use std::sync::mpsc::{self,Sender,Receiver};
 use std::{time, thread};
+use std::net::{TcpListener, TcpStream};
 
-use rand::Rng;
 use piston::window::WindowSettings;
 use piston::event_loop::*;
 use piston::input::*;
@@ -18,6 +18,33 @@ use opengl_graphics::{ GlGraphics, OpenGL, TextureSettings };
 
 
 type FrameBuffer = Arc<Mutex<im::RgbaImage>>;
+
+#[derive(Debug)]
+struct Pixel {
+    x: u16,
+    y: u16,
+    r: u8,
+    g: u8,
+    b: u8,
+    a: u8,
+}
+
+impl Pixel {
+    fn from_slice(buffer: &[u8]) -> Pixel {
+        use std::io::Cursor;
+        use byteorder::{NetworkEndian, ReadBytesExt};
+        let mut rdr = Cursor::new(buffer);
+        Pixel {
+            x : rdr.read_u16::<NetworkEndian>().unwrap(),
+            y : rdr.read_u16::<NetworkEndian>().unwrap(),
+            r : rdr.read_u8().unwrap(),
+            g : rdr.read_u8().unwrap(),
+            b : rdr.read_u8().unwrap(),
+            a : 255,
+        }
+    }
+}
+
 
 struct App {
 	gl: GlGraphics,
@@ -68,23 +95,44 @@ fn render(tx: Sender<Command>, frame_buffer: FrameBuffer) {
     tx.send(Command::Shutdown).unwrap();
 }
 
-fn paint(frame_buffer: FrameBuffer) {
-    let sleep_time = time::Duration::from_millis(2);
 
-    const CYAN: [u8; 4] = [0, 255, 255, 255];
+fn handle_client(mut stream: TcpStream, frame_buffer: FrameBuffer) {
+	use std::io::Read;
+	loop {
+		let mut net_buffer: [u8; 7] = [0; 7];
+		let res = stream.read_exact(&mut net_buffer);
+		match res {
+			Ok(_) => {
+                let pixel = Pixel::from_slice(&net_buffer);
+                println!("{:?}", pixel);
+                let mut fb = frame_buffer.lock().unwrap();
+                let (width, height) = fb.dimensions();
 
-	let mut i: u32 = 0;
-	let mut rng = rand::thread_rng();
-    loop {
-        thread::sleep(sleep_time);
-		let mut fb = frame_buffer.lock().unwrap();
-        let (width, height) = fb.dimensions();
-        let (x, y) = (rng.gen::<u32>() % width, rng.gen::<u32>() % height);
+                fb.put_pixel((pixel.x as u32 % width), (pixel.y as u32 % height),
+                             im::Rgba([pixel.r, pixel.g, pixel.b, pixel.a]));
 
-        fb.put_pixel(x, y,
-			im::Rgba([(x % 255) as u8, (y % 255) as u8, rng.gen(), 255]));
-		i = (i + 1) % std::cmp::min(width, height);
-    }
+			},
+			Err(_) => {
+				break;
+			},
+
+		}
+	}
+}
+
+fn listener(frame_buffer: FrameBuffer) {
+	let listener = TcpListener::bind("127.0.0.1:1234").unwrap();
+	for stream in listener.incoming() {
+        let buffer_ref = frame_buffer.clone();
+		match stream {
+			Ok(stream) => {
+				thread::spawn(move || {
+					handle_client(stream, buffer_ref)
+				});
+			}
+			Err(_) => { panic!("Connection failed") }
+		}
+	}
 }
 
 enum Command {
@@ -101,9 +149,10 @@ fn main() {
         render(tx_ref, buffer_ref);
     });
 
+
     let buffer_ref = frame_buffer.clone();
     thread::spawn(move || {
-        paint(buffer_ref);
+        listener(buffer_ref);
     });
 
     // Add a command queue
