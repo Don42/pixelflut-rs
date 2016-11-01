62988e35e253db101e5579437f8f3c3a8a805dec
Marco 'don' Kaulea <DonMarco42@gmail.com>
2016-11-01 15:34:53 +0100

    Run rustfmt

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ extern crate image as im;
 extern crate byteorder;
 
 use std::sync::{Arc, Mutex};
-use std::sync::mpsc::{self,Sender,Receiver};
+use std::sync::mpsc::{self, Sender, Receiver};
 use std::{time, thread};
 use std::net::{TcpListener, TcpStream};
 
@@ -14,7 +14,7 @@ use piston::window::WindowSettings;
 use piston::event_loop::*;
 use piston::input::*;
 use glutin_window::GlutinWindow as Window;
-use opengl_graphics::{ GlGraphics, OpenGL, TextureSettings };
+use opengl_graphics::{GlGraphics, OpenGL, TextureSettings};
 
 
 type FrameBuffer = Arc<Mutex<im::RgbaImage>>;
@@ -35,12 +35,12 @@ impl Pixel {
         use byteorder::{NetworkEndian, ReadBytesExt};
         let mut rdr = Cursor::new(buffer);
         Pixel {
-            x : rdr.read_u16::<NetworkEndian>().unwrap(),
-            y : rdr.read_u16::<NetworkEndian>().unwrap(),
-            r : rdr.read_u8().unwrap(),
-            g : rdr.read_u8().unwrap(),
-            b : rdr.read_u8().unwrap(),
-            a : 255,
+            x: rdr.read_u16::<NetworkEndian>().unwrap(),
+            y: rdr.read_u16::<NetworkEndian>().unwrap(),
+            r: rdr.read_u8().unwrap(),
+            g: rdr.read_u8().unwrap(),
+            b: rdr.read_u8().unwrap(),
+            a: 255,
         }
     }
 
@@ -51,93 +51,90 @@ impl Pixel {
 
 
 struct App {
-	gl: GlGraphics,
+    gl: GlGraphics,
     frame_buffer: FrameBuffer,
 }
 
 impl App {
-	fn render(&mut self, args: &RenderArgs) {
+    fn render(&mut self, args: &RenderArgs) {
 
         let frame_buffer = self.frame_buffer.lock().unwrap();
 
-		const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
-        let mut  texture = opengl_graphics::Texture::from_image(
-            &frame_buffer,
-            &TextureSettings::new());
-		self.gl.draw(args.viewport(), |c, gl| {
-			graphics::clear(BLACK, gl);
+        const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
+        let mut texture = opengl_graphics::Texture::from_image(&frame_buffer,
+                                                               &TextureSettings::new());
+        self.gl.draw(args.viewport(), |c, gl| {
+            graphics::clear(BLACK, gl);
             texture.update(&frame_buffer);
             graphics::image(&texture, c.transform, gl);
-		});
+        });
     }
 
-	fn update(&mut self, args: &UpdateArgs) {
-	}
+    fn update(&mut self, args: &UpdateArgs) {}
 }
 
 fn render(tx: Sender<Command>, frame_buffer: FrameBuffer) {
 
-	let opengl = OpenGL::V3_2;
+    let opengl = OpenGL::V3_2;
     let mut window: Window = WindowSettings::new("piston", [512; 2])
-		.exit_on_esc(true)
-		.opengl(opengl)
+        .exit_on_esc(true)
+        .opengl(opengl)
         .build()
         .unwrap();
 
-	let mut app = App {
+    let mut app = App {
         gl: GlGraphics::new(opengl),
         frame_buffer: frame_buffer,
-        };
+    };
 
 
-	let mut events = window.events();
+    let mut events = window.events();
     while let Some(e) = events.next(&mut window) {
-		if let Some(r) = e.render_args() {
-			app.render(&r);
-		}
+        if let Some(r) = e.render_args() {
+            app.render(&r);
+        }
     }
     tx.send(Command::Shutdown).unwrap();
 }
 
 
 fn handle_client(mut stream: TcpStream, frame_buffer: FrameBuffer) {
-	use std::io::Read;
-	loop {
-		let mut net_buffer: [u8; 7] = [0; 7];
-		let res = stream.read_exact(&mut net_buffer);
-		match res {
-			Ok(_) => {
+    use std::io::Read;
+    loop {
+        let mut net_buffer: [u8; 7] = [0; 7];
+        let res = stream.read_exact(&mut net_buffer);
+        match res {
+            Ok(_) => {
                 let pixel = Pixel::from_slice(&net_buffer);
                 let mut fb = frame_buffer.lock().unwrap();
                 let (width, height) = fb.dimensions();
 
                 if pixel.is_on_canvas(width, height) {
-                    fb.put_pixel((pixel.x as u32), (pixel.y as u32),
-                             im::Rgba([pixel.r, pixel.g, pixel.b, pixel.a]));
+                    fb.put_pixel((pixel.x as u32),
+                                 (pixel.y as u32),
+                                 im::Rgba([pixel.r, pixel.g, pixel.b, pixel.a]));
                 };
 
-			},
-			Err(_) => {
-				break;
-			},
+            }
+            Err(_) => {
+                break;
+            }
 
-		}
-	}
+        }
+    }
 }
 
 fn listener(frame_buffer: FrameBuffer) {
-	let listener = TcpListener::bind("127.0.0.1:1234").unwrap();
-	for stream in listener.incoming() {
+    let listener = TcpListener::bind("127.0.0.1:1234").unwrap();
+    for stream in listener.incoming() {
         let buffer_ref = frame_buffer.clone();
-		match stream {
-			Ok(stream) => {
-				thread::spawn(move || {
-					handle_client(stream, buffer_ref)
-				});
-			}
-			Err(_) => { panic!("Connection failed") }
-		}
-	}
+        match stream {
+            Ok(stream) => {
+                thread::spawn(move || handle_client(stream, buffer_ref));
+            }
+            Err(_) => panic!("Connection failed"),
+        }
+    }
 }
 
 enum Command {
